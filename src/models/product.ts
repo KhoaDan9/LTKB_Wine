4c66c0d598305ceb32c88c47022801e141974e95
Hocsinhgioiit <104492035+Hocsinhgioiit@users.noreply.github.com>
2023-06-14 07:09:16 +0700

    admin

--- src/models/product.ts
+++ src/models/product.ts
@@ -5,7 +5,10 @@ const productSchema = new Schema(
     name: { type: String, required: true },
     origin: { type: String },
     price: { type: Number, required: true },
-    description: { type: String }
+    description: { type: String },
+    quantity: { type: Number },
+    type: { type: String },
+    imgsrc: { type: String, required: true }
   },
   { timestamps: true }
 )
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Application } from 'express'
 import userRoute from './user'
 import homeRoute from './home'
-
+import adminRoute from './admin'
 function route(app: Application) {
   app.use('/auth', userRoute)
   app.use('/', homeRoute)
+  app.use('/admin', adminRoute)
 }
 
 export default route
