bc6192df2d5b36b8ffc8b91d44a9981e2532609e
KhoaDan9 <khoadangx2@gmail.com>
2023-06-17 22:27:08 +0700

    up validate + fix fe

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,6 +3,7 @@ import { User } from '../models/user'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
+import { validationResult } from 'express-validator'
 dotenv.config()
 
 const tokenKey = process.env.TOKEN_KEY as string
@@ -10,30 +11,36 @@ const tokenKey = process.env.TOKEN_KEY as string
 export class AuthController {
   async register(req: Request, res: Response) {
     try {
-      const { username, password, fullname, address, phone } = req.body
+      const { username, password1, password2, fullname, address, phone } = req.body
 
-      const oldUser = await User.findOne({ username })
-
-      if (oldUser) return res.render('register', { user_exists: true, hideNavbar: true, hideSearchBar: true })
+      const errors = validationResult(req)
+      if (!errors.isEmpty())
+        res.render('register', { hideNavbar: true, hideSearchBar: true, errors: errors.array()[0] })
+      else if (password1 !== password2)
+        res.render('register', { hideNavbar: true, hideSearchBar: true, password_wrong: true })
       else {
-        const encryptedPassword = await bcrypt.hash(password, 10)
-        const user = await User.create({
-          username,
-          password: encryptedPassword,
-          fullname,
-          address,
-          phone
-        })
-        const token = jwt.sign({ user_id: user._id, username }, tokenKey, {
-          expiresIn: '2h'
-        })
-        user.token = token
-        user.save()
+        const oldUser = await User.findOne({ username })
+        if (oldUser) return res.render('register', { user_exists: true, hideNavbar: true, hideSearchBar: true })
+        else {
+          const encryptedPassword = await bcrypt.hash(password1, 10)
+          const user = await User.create({
+            username,
+            password: encryptedPassword,
+            fullname,
+            address,
+            phone
+          })
+          const token = jwt.sign({ user_id: user._id, username }, tokenKey, {
+            expiresIn: '2h'
+          })
+          user.token = token
+          user.save()
 
-        res.redirect('/auth/login')
+          res.redirect('/auth/login')
+        }
       }
     } catch (err) {
-      res.send('err')
+      res.send('err' + err)
     }
   }
 
@@ -47,21 +54,22 @@ export class AuthController {
         httpOnly: true // The cookie only accessible by the web server
       }
 
-      if (!(username && password)) {
-        res.status(400).send('All input is required')
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) res.render('login', { hideNavbar: true, hideSearchBar: true, errors: errors.array()[0] })
+      else {
+        const user = await User.findOne({ username })
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+          const token = jwt.sign({ user_id: user._id, username }, tokenKey, {
+            expiresIn: '2h'
+          })
+
+          user.token = token
+          user.save()
+          res.cookie('x-access-token', token, options)
+          res.redirect('/')
+        } else res.render('login', { incorrect: true, hideNavbar: true, hideSearchBar: true })
       }
-      const user = await User.findOne({ username })
-
-      if (user && (await bcrypt.compare(password, user.password))) {
-        const token = jwt.sign({ user_id: user._id, username }, tokenKey, {
-          expiresIn: '2h'
-        })
-
-        user.token = token
-        user.save()
-        res.cookie('x-access-token', token, options)
-        res.redirect('/')
-      } else res.render('login', { incorrect: true, hideNavbar: true, hideSearchBar: true })
     } catch (err) {
       res.send(err)
     }
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express'
 
 import { UserController } from '~/controllers/userController'
+import { resetPassword, userUpdateValidation } from '~/middlewares/validate'
 
 const router = Router()
 const UserController1 = new UserController()
 
 router.get('/', UserController1.home)
 router.get('/reset-password', UserController1.getResetPassword)
-router.post('/update', UserController1.update)
-router.post('/reset-password', UserController1.resetPassword)
+router.post('/update', userUpdateValidation(), UserController1.update)
+router.post('/reset-password', resetPassword(), UserController1.resetPassword)
 
 export default router
