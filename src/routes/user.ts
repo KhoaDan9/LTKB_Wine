70d184ac85db5134cf186c6137cb0038c5844613
KhoaDan9 <khoadangx2@gmail.com>
2023-09-28 23:34:14 +0700

    Update submit userid

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -8,7 +8,21 @@ import {
   addCreditcard,
   addBankAccount
 } from '~/middlewares/validate'
+import multer from 'multer'
+import path from 'path'
 
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve('src/public/uploads'))
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
+
+const upload = multer({ storage: storage })
 const router = Router()
 const UserController1 = new UserController()
 
@@ -25,5 +39,7 @@ router.get('/add-creditcard', UserController1.creditcard)
 router.post('/add-creditcard', addCreditcard(), UserController1.addCreditCard)
 router.get('/add-bankaccount', UserController1.bankaccount)
 router.post('/add-bankaccount', addBankAccount(), UserController1.addBankAccount)
+router.get('/add-userid', UserController1.userid)
+router.post('/add-userid', upload.single('userid'), UserController1.addUserId)
 
 export default router
